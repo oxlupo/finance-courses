bc1347193dc99e35ee31a1aceef5a02eed8baa3a
yousef <mr.ysaj@gmail.com>
2022-05-05 14:03:13 +0430

    [UPDATE] was updated

--- sample.py
+++ sample.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import train.edhec_risk_kit as erk
-
+import matplotlib.pyplot as plt
 e = erk.get_ind_returns()
-print(e)
+# print(e)
 ind = erk.get_ind_returns()
 er = erk.annualize_rets(ind['1996': "2000"], 12)
 
@@ -15,12 +15,14 @@ def gbm(n_years=10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_
     Evolution of stock Price using a Geometric Brownian Motion Model
     """
     dt = 1/steps_per_year
-    n_steps = int(n_years ** steps_per_year)
-    xi = np.random.normal(size=(n_steps, n_scenarios))
-    rets = mu * dt + sigma*np.sqrt(dt) * xi
-    rets = pd.DataFrame(rets)
-    prices = s_0 * (1 + rets).cumprod()
+    n_steps = int(n_years * steps_per_year)
+    rets_plus_1 = np.random.normal(loc=(1+ mu*dt), scale=(sigma * np.sqrt(dt)), size=(n_steps, n_scenarios))
+
+    prices = s_0 * pd.DataFrame(rets_plus_1).cumprod()
+
     return prices
 
 p = gbm()
 print(p)
+p.plot(legend=False)
+plt.show()
